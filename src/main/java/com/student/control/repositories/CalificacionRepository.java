f5255ad7e5e56dcd1f860fd467f6242506f38541
JefferGonzalez <jefferd2016@gmail.com>
2023-05-22 12:25:43 -0500

    Update

---
+++ src/main/java/com/student/control/repositories/CalificacionRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.student.control.repositories;
+
+import com.student.control.models.Corte;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author SEBASTIAN L
+ */
+public interface PeriodoRepository extends JpaRepository<Corte, Integer >{
+    
+}
--- src/main/java/com/student/control/repositories/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.student.control.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.student.control.models.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-
-}
